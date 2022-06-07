aa2c2fa673ed67643264c830ab8353d1965f225d
Kyle Vance <91154892+KVance1010@users.noreply.github.com>
2022-06-07 12:01:24 -0700

    add subject text bar functional now

--- src/main/java/com/learnersacademy/controller/CourseListController.java
+++ src/main/java/com/learnersacademy/controller/CourseListController.java
@@ -6,9 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.learnersacademy.dao.CourseRepository;
 import com.learnersacademy.dao.SubjectRepository;
@@ -20,50 +20,66 @@ public class CourseListController {
 
 	@Autowired
 	CourseRepository courseRepository;
-	
+
 	@Autowired
 	SubjectRepository subjectRepository;
 
-	// mapping HTTP GET requests
+	// Main list of courses and subjects
 	@GetMapping("/courses-subjects")
 	public String listCoursesAndSubjects(Model model) {
-		
+
 		List<Course> course = courseRepository.findAll();
-		
-		if (course != null) { model.addAttribute("condition2", Boolean.TRUE);}
-		else { model.addAttribute("condition2", Boolean.FALSE);}
-		
+
+		if (course != null) {
+			model.addAttribute("condition2", Boolean.TRUE);
+		} else {
+			model.addAttribute("condition2", Boolean.FALSE);
+		}
+
 		List<Subject> subject = subjectRepository.findAll();
-		
-		if (subject != null) { model.addAttribute("condition1", Boolean.TRUE);}
-		else { model.addAttribute("condition1", Boolean.FALSE);}
-		
+
+		if (subject != null) {
+			model.addAttribute("condition1", Boolean.TRUE);
+		} else {
+			model.addAttribute("condition1", Boolean.FALSE);
+		}
+
 		model.addAttribute("subject", subject);
 		model.addAttribute("course", course);
 		return "/courseSubject/course-subject-list";
 	}
 
-	// handler method to handle delete student request
-		@GetMapping("/subject/{subjectId}")
-		public String deleteSubject(@PathVariable Long subjectId) {
-			subjectRepository.deleteById(subjectId);
-			return "redirect:/courses-subjects";
-		}
-		
-		@GetMapping("/new-course")
-		public String createCourse(Model model) {          
-			Course course = new Course();
-			List<Subject> subject = subjectRepository.findAll();
-			model.addAttribute("course", course);              
-			model.addAttribute("subject", subject);       
-			return "/courseSubject/create-course"; 
-		}
-		
-		@PostMapping("/course/save")
-		public String createCourse(Course course, Model model){
-			courseRepository.save(course);
-			return "redirect:/courses-subjects";
-		}
-		
-		
+	// Delete a Subject
+	@GetMapping("/subject-delete/{subjectId}")
+	public String deleteSubject(@PathVariable Long subjectId) {
+		subjectRepository.deleteById(subjectId);
+		return "redirect:/courses-subjects";
+	}
+
+	// Add a Subject
+	@GetMapping("/subject-save")
+	public String addSubject(@RequestParam String subjectName ) {
+		Subject subject = new Subject();
+		subject.setSubjectName(subjectName);
+		subjectRepository.save(subject);
+		return "redirect:/courses-subjects";
+	}
+
+	// create a course
+	@GetMapping("/new-course")
+	public String createCourse(Model model) {
+		Course course = new Course();
+		List<Subject> subject = subjectRepository.findAll();
+		model.addAttribute("course", course);
+		model.addAttribute("subject", subject);
+		return "/courseSubject/create-course";
+	}
+
+	// save the course
+	@PostMapping("/course/save")
+	public String createCourse(Course course, Model model) {
+		courseRepository.save(course);
+		return "redirect:/courses-subjects";
+	}
+
 }
